0202f7accac023f3060ab428cc94bd34dc3e54a1
Slava Kuryaev <92830706+necrosskull@users.noreply.github.com>
2023-09-24 22:56:26 +0300

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -10,6 +10,7 @@ import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def main():
